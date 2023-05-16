f5911e898d9db792702b7a3c9b1f4abfa921eea2
Isabel Sánchez <isasancas@alum.us.es>
2023-05-16 20:56:27 +0200

    Entrega 3

--- src/fp/atletas/test/TestFactoriaAtletas.java
+++ src/fp/atletas/test/TestFactoriaAtletas.java
@@ -7,11 +7,19 @@ public class TestFactoriaAtletas {
 
 	public static void main(String[] args) {
 		testLeerAtletas("data/athlete_events.csv");
+		testLeerAtletasStream("data/athlete_events.csv");
+		
 	}
 
 	private static void testLeerAtletas(String fichero) {
 		System.out.println("\nTestLeerAtletas =============");
 		Atletas atletas = FactoriaAtletas.leerAtletas(fichero);
+		System.out.println("Atletas: "+ atletas);
+	}
+	
+	private static void testLeerAtletasStream(String fichero) {
+		System.out.println("\nTestLeerAtletas =============");
+		Atletas atletas = FactoriaAtletas.leerAtletasStream(fichero);
 		System.out.println("Atletas: "+ atletas);
 	}
 
